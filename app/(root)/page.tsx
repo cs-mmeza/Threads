f43ea8483790ac838e84ad49a2a2ab14faae2f32
cs-mmeza <mmeza1701@gmail.com>
2024-02-28 23:39:41 -0500

    add:     Topbar, Leftsidebar, assets.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
     <div className="">
-      <UserButton afterSignOutUrl="/"/>
+      <h1 className="head-text text-left">Home</h1>      
     </div>
   )
 }
